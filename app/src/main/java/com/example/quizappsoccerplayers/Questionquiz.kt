15c9a74ae5f204b5a1d541c73311c5e3831ceb93
Juesgape <juesgape11@gmail.com>
2022-09-07 17:16:09 -0500

    Adding functions and onclick listener for selected buttons

--- app/src/main/java/com/example/quizappsoccerplayers/Questionquiz.kt
+++ app/src/main/java/com/example/quizappsoccerplayers/Questionquiz.kt
@@ -10,7 +10,6 @@ class Questionquiz : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_questionquiz)
 
     }
 }
--- app/src/main/java/com/example/quizappsoccerplayers/QuizQuestion.kt
+++ app/src/main/java/com/example/quizappsoccerplayers/QuizQuestion.kt
@@ -1,33 +1,108 @@
 package com.example.quizappsoccerplayers
 
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 
-class QuizQuestion : AppCompatActivity() {
+class QuizQuestion : AppCompatActivity(), View.OnClickListener {
+
+    private var mCurrentPosition:Int = 1
+    private var mQuestionList:ArrayList<Question>? = null
+    private var mSelectedOptionPosition:Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_questionquiz)
 
+        //Finding elements by ID
+        val tv_option_one = findViewById<TextView>(R.id.tv_option_one)
+        val tv_option_two = findViewById<TextView>(R.id.tv_option_two)
+        val tv_option_three = findViewById<TextView>(R.id.tv_option_three)
+
+
+        mQuestionList = Constants.getQuestions()
+
+        setQuestion()
+
+        tv_option_one.setOnClickListener(this)
+        tv_option_two.setOnClickListener(this)
+        tv_option_three.setOnClickListener(this)
+    }
+
+    private fun setQuestion() {
+
+        mCurrentPosition = 1
+        val question = mQuestionList!![mCurrentPosition-1]
+
+        defaultOptionsView()
+
+        //Finding elements by ID
         val tv_question = findViewById<TextView>(R.id.tv_question)
         val iv_image = findViewById<ImageView>(R.id.iv_image)
         val tv_option_one = findViewById<TextView>(R.id.tv_option_one)
         val tv_option_two = findViewById<TextView>(R.id.tv_option_two)
         val tv_option_three = findViewById<TextView>(R.id.tv_option_three)
 
-
-        val questionsList = Constants.getQuestions()
-        Log.i("Questions size ", "${questionsList.size}")
-
-        val currentPosition = 1
-        val question: Question? = questionsList[currentPosition -1]
-
         tv_question.text = question!!.question
         iv_image.setImageResource(question.image)
         tv_option_one.text = question.optionOne
         tv_option_two.text = question.optionTwo
         tv_option_three.text = question.optionThree
+
     }
+
+    private fun defaultOptionsView() {
+        //Finding elements by ID
+        val tv_option_one = findViewById<TextView>(R.id.tv_option_one)
+        val tv_option_two = findViewById<TextView>(R.id.tv_option_two)
+        val tv_option_three = findViewById<TextView>(R.id.tv_option_three)
+
+        val options = ArrayList<TextView>()
+        options.add(0, tv_option_one)
+        options.add(1, tv_option_two)
+        options.add(2, tv_option_three)
+
+        //idk about this
+        for(option in options) {
+            option.setTextColor(Color.parseColor("#7A8089"))
+            option.typeface = Typeface.DEFAULT
+            option.background = ContextCompat.getDrawable(this, R.drawable.default_option_button)
+        }
+    }
+
+    override fun onClick(v: View?) {
+
+        //Finding elements by ID
+        val tv_option_one = findViewById<TextView>(R.id.tv_option_one)
+        val tv_option_two = findViewById<TextView>(R.id.tv_option_two)
+        val tv_option_three = findViewById<TextView>(R.id.tv_option_three)
+
+        when(v?.id) {
+            R.id.tv_option_one -> {
+                selectedOptionView(tv_option_one, 1)
+            }
+            R.id.tv_option_two -> {
+                selectedOptionView(tv_option_two, 2)
+            }
+            R.id.tv_option_three -> {
+                selectedOptionView(tv_option_three, 3)
+            }
+        }
+    }
+
+    private fun selectedOptionView(tv: TextView, selectedOptionNum: Int) {
+        defaultOptionsView()
+        mSelectedOptionPosition = selectedOptionNum
+
+        tv.setTextColor(Color.parseColor("#363A43"))
+        tv.setTypeface(tv.typeface, Typeface.BOLD)
+        tv.background = ContextCompat.getDrawable(this,
+            R .drawable.selected_option_border)
+    }
+
 }
